748b37e6b86c998cf69a17e9eefa207467b669cc
shogo <peshogo@gmail.com>
2017-11-06 21:01:39 +0900

    Tutorial 1

---
+++ src/Tutorial1.kt
@@ -0,0 +1,18 @@
+fun main(args: Array<String>) {
+    val hello = "Hello World!"
+    println(hello)
+
+    val i: Int = 10
+    val j = 20
+
+    val s = "hogehoge"
+
+    // val num = 0
+    // num = 1 <- Error
+
+    var num = 0
+    num = 1 // <- OK
+
+    println("${i + j}")
+
+}
