078de0abbd3e28562736e1d23c6e8f3d20582704
shogo <peshogo@gmail.com>
2017-11-06 21:04:18 +0900

    Tutorial 1

--- src/Tutorial1.kt
+++ src/Tutorial1.kt
@@ -2,8 +2,8 @@ fun main(args: Array<String>) {
     val hello = "Hello World!"
     println(hello)
 
-    val i: Int = 10
-    val j = 20
+    // val i: Int = 10
+    val i = 20
 
     val s = "hogehoge"
 
@@ -13,6 +13,16 @@ fun main(args: Array<String>) {
     var num = 0
     num = 1 // <- OK
 
-    println("${i + j}")
+    val a = 100
+    val b = 50
 
+    // 普通にprintln()を使って出力できます
+    println(a)
+    println(a + b)
+
+    // 文字列の途中に変数を入れることもできます
+    println("a + b = ${a + b}")
+
+    // ${}の中が一つの場合$だけに省略できます
+    println("a = $a")
 }
